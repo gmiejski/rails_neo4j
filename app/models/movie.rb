1d768709dc5cb6eab63505e5c2731d334d1fcf1d
Grzegorz Miejski <grz.miejski@gmail.com>
2014-06-09 20:19:29 +0200

    Creating Like node with every like.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,25 +12,31 @@ class Movie < ActiveRecord::Base
     raise 'movie_id cannot be null' if movie_id.nil?
     like = Like.create(user_id: user_id)
     @neo = Neography::Rest.new
+    like_node = @neo.create_node(id: like.id)
+    @neo.add_label(like_node, 'Like')
+
     movie_node = @neo.find_nodes_labeled('Movie', {:id => movie_id})
     user_node = @neo.find_nodes_labeled('User', {:id => user_id})
     raise 'user node not found' if user_node.nil?
     raise 'movie node not found' if user_node.nil?
-    node = @neo.create_relationship('likes', user_node, movie_node)
+    relationship = @neo.create_relationship('likes', user_node, movie_node)
+    @neo.set_relationship_properties(relationship, {:id => like.id})
   }
 
   def add_like (user_id)
     raise 'user_id cannot be null' if user_id.nil?
     like = Like.create(user_id: user_id)
     @neo = Neography::Rest.new
-    puts self.id
+    like_node = @neo.create_node(id: like.id)
+    @neo.add_label(like_node, 'Like')
+
     movie_node = @neo.find_nodes_labeled('Movie', {:id => self.id})
     user_node = @neo.find_nodes_labeled('User', {:id => user_id})
     raise 'user node not found' if user_node.nil?
     raise 'movie node not found' if user_node.nil?
-    node = @neo.create_relationship('likes', user_node, movie_node)
+    relationship = @neo.create_relationship('likes', user_node, movie_node)
+    @neo.set_relationship_properties(relationship, {:id => like.id})
   end
-
 
   def likers
     puts 'getting likes from movie'
