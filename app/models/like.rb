8e3c514b81c79ddd55fdfc9a52f6fe1fc0e30800
Grzegorz Miejski <grz.miejski@gmail.com>
2014-06-09 21:18:12 +0200

    Like a like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,21 @@
 class Like < ActiveRecord::Base
   belongs_to :user
+
+  scope :add_like, ->(user_id, like_id) {
+    raise 'like_id cannot be null' if like_id.nil?
+    raise 'user_id cannot be null' if user_id.nil?
+    like = Like.create(user_id: user_id)
+    @neo = Neography::Rest.new
+    like_node = @neo.create_node(id: like.id)
+    @neo.add_label(like_node, 'Like')
+
+    like_node = @neo.find_nodes_labeled('Like', {:id => like_id})
+    user_node = @neo.find_nodes_labeled('User', {:id => user_id})
+    raise 'user node not found' if user_node.nil?
+    raise 'like node not found' if like_node.nil?
+    relationship = @neo.create_relationship('likes', user_node, like_node)
+    @neo.set_relationship_properties(relationship, {:id => like.id})
+    'you liked this like!'
+  }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_many :likes
+
+  scope :create_neo, ->(name) {
+    raise 'name cannot be null' if name.nil?
+
+    user = User.create(name: name)
+    @neo = Neography::Rest.new
+    node = @neo.create_node(id: user.id)
+    @neo.add_label(node, 'User')
+  }
+
 end
