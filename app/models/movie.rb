f1a0ba73a59d545f5ed6b3195af06b6ee83919c7
Grzegorz Miejski <grz.miejski@gmail.com>
2014-06-09 19:52:26 +0200

    Seeds completed with neo4j.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,14 +3,20 @@ class Movie < ActiveRecord::Base
   scope :create_neo, ->(title, director) do
     movie = Movie.create(title: title, director: director)
     @neo = Neography::Rest.new
-    @neo.create_node('id' => movie.id, title: movie.title, director: movie.director)
+    node = @neo.create_node(id: movie.id, title: movie.title, director: movie.director)
+    @neo.add_label(node, 'Movie')
   end
 
   scope :add_like, ->(user_id) do
-    return if user_id.nil?
+    raise 'user_id cannot be null' if user_id.nil?
     like = Like.create(user_id: user_id)
     @neo = Neography::Rest.new
-    #node1 = @neo.create_node(id=)
+    movie_node = @neo.find_nodes_labeled('Movie', {:id => self.id})
+    user_node = @neo.find_nodes_labeled('User', {:id => user_id})
+    raise 'user node not found' if user_node.nil?
+    raise 'movie node not found' if user_node.nil?
+    node = @neo.create_relationship('likes', movie_node, user_node)
+
   end
 
   def likers
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: 'Grzesiek M.')
-User.create(name: 'Krzysiek T.')
+@neo = Neography::Rest.new
+
+u1 = User.create(name: 'Grzesiek M.')
+u1_node = @neo.create_node(id: u1.id, name: u1.name)
+u2 = User.create(name: 'Krzysiek T.')
+u2_node = @neo.create_node(id: u2.id, name: u2.name)
+@neo.add_label(u1_node, 'User')
+@neo.add_label(u2_node, 'User')
+
 #User.create(name: 'Krzysiek R.')
 #User.create(name: 'Krzysiek Z.')
 #User.create(name: 'Krzysiek E.')
 #User.create(name: 'Krzysiek P.')
-User.create(name: 'Łukasz S.')
+#User.create(name: 'Łukasz S.')
 
-Movie.create(title: 'Matrix', director: 'Ktoś tam')
-Movie.create(title: 'Matrix 2', director: 'Ktoś tam')
+m1 = Movie.create(title: 'Matrix', director: 'Ktoś tam')
+m2 = Movie.create(title: 'Matrix 2', director: 'Ktoś tam')
+
+m1_node = @neo.create_node(id: m1.id, title: m1.title, director: m1.director)
+m2_node = @neo.create_node(id: m2.id, title: m2.title, director: m2.director)
+
+@neo.add_label(m1_node, 'Movie')
+@neo.add_label(m2_node, 'Movie')
+
