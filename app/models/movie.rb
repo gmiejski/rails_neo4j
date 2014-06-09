24cd6e20c9d1f858111a21a6ac2d439eed4a3d25
Grzegorz Miejski <grz.miejski@gmail.com>
2014-06-09 20:01:51 +0200

    Movie liking added.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,17 +7,30 @@ class Movie < ActiveRecord::Base
     @neo.add_label(node, 'Movie')
   end
 
-  scope :add_like, ->(user_id) do
+  scope :add_like, ->(movie_id, user_id) do
+    raise 'user_id cannot be null' if user_id.nil?
+    raise 'movie_id cannot be null' if movie_id.nil?
+    like = Like.create(user_id: user_id)
+    @neo = Neography::Rest.new
+    movie_node = @neo.find_nodes_labeled('Movie', {:id => movie_id})
+    user_node = @neo.find_nodes_labeled('User', {:id => user_id})
+    raise 'user node not found' if user_node.nil?
+    raise 'movie node not found' if user_node.nil?
+    node = @neo.create_relationship('likes', user_node, movie_node)
+  end
+
+  def add_like ( user_id)
     raise 'user_id cannot be null' if user_id.nil?
     like = Like.create(user_id: user_id)
     @neo = Neography::Rest.new
+    puts self.id
     movie_node = @neo.find_nodes_labeled('Movie', {:id => self.id})
     user_node = @neo.find_nodes_labeled('User', {:id => user_id})
     raise 'user node not found' if user_node.nil?
     raise 'movie node not found' if user_node.nil?
-    node = @neo.create_relationship('likes', movie_node, user_node)
-
+    node = @neo.create_relationship('likes', user_node, movie_node)
   end
+
 
   def likers
     puts 'getting likes from movie'
