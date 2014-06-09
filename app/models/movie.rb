090c3701f2a271c90b7d396116812c6686730758
Grzegorz Miejski <grz.miejski@gmail.com>
2014-06-09 20:03:55 +0200

    Deleted scope methods warnings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,13 @@
 class Movie < ActiveRecord::Base
 
-  scope :create_neo, ->(title, director) do
+  scope :create_neo, ->(title, director) {
     movie = Movie.create(title: title, director: director)
     @neo = Neography::Rest.new
     node = @neo.create_node(id: movie.id, title: movie.title, director: movie.director)
     @neo.add_label(node, 'Movie')
-  end
+  }
 
-  scope :add_like, ->(movie_id, user_id) do
+  scope :add_like, ->(movie_id, user_id) {
     raise 'user_id cannot be null' if user_id.nil?
     raise 'movie_id cannot be null' if movie_id.nil?
     like = Like.create(user_id: user_id)
@@ -17,9 +17,9 @@ class Movie < ActiveRecord::Base
     raise 'user node not found' if user_node.nil?
     raise 'movie node not found' if user_node.nil?
     node = @neo.create_relationship('likes', user_node, movie_node)
-  end
+  }
 
-  def add_like ( user_id)
+  def add_like (user_id)
     raise 'user_id cannot be null' if user_id.nil?
     like = Like.create(user_id: user_id)
     @neo = Neography::Rest.new
